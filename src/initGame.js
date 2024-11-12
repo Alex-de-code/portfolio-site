a8a4ed47422f7aa477684d3270b408e05cd53cc1
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-11-12 01:55:00 -0500

    imported makeKaplayCtx into initGame.js

--- src/initGame.js
+++ src/initGame.js
@@ -1,3 +1,5 @@
+import makeKaplayCtx from "./kaplayCtx";
+
 export default async function initGame() {
   const k = makeKaplayCtx(); // holds Kaplay Contexy
   k.loadSprite("player", "./sprites/player.png", {}); // loadSprite() is a f(x) kaplay offers, 1st parameter is used by specifying a key for a specific sprite, 2nd is that sprite's path, 3rd is data properties
