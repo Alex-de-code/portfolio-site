43f17dad085e44aa6e1755b1b72be4c532cc61e0
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-11-12 14:24:16 -0500

    found new color pallette to try

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
 export const PALETTE = {
   color1: "#272946",
-  color2: "#aaea6c",
-  color3: "#e7ffee",
+  color2: "#0ce6f2", // rich baby blue
+  color3: "#e7ffee", // greenish white
 };
+
+//lospec blue color pallette
+//https://lospec.com/palette-list/eulbink
