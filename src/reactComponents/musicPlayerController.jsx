49c407f18303393dc54e39bc3971858a1282229e
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-11-17 23:13:07 -0500

    implemented ticker functionality into radio

--- src/reactComponents/musicPlayerController.jsx
+++ src/reactComponents/musicPlayerController.jsx
@@ -1,25 +1,6 @@
 import { useEffect, useState } from "react";
 
 export default function musicPlayerController() {
-  //   const date = new Date();
-  //   const offsetInHours = date.getTimezoneOffset() / 60;
-  //   const currentTime = date.toLocaleTimeString();
-  //   const timeZone = () => {
-  //     offsetInHours > 0
-  //       ? "UTC+" + offsetInHours
-  //       : "UTC-" + Math.abs(offsetInHours);
-  //     return `${currentTime} (${timeZone})`;
-  //   };
-
-  //   const getCurrentTime = () => {
-  //     const now = new Date();
-  //     const hours = now.getHours();
-  //     const minutes = now.getMinutes();
-  //     const seconds = now.getSeconds();
-  //     return `${hours}:${minutes}:${seconds}`;
-  //   };
-
-  //   console.log(getCurrentTime());
   const [dateState, setDateState] = useState(new Date());
   useEffect(() => {
     setInterval(() => setDateState(new Date()), 30000);
@@ -30,7 +11,7 @@ export default function musicPlayerController() {
       <div className="bg-red-400 absolute bottom-5 left-5 rounded-xl md:w-1/5 lg:h-1/6 p-1 lg:p-3">
         <div className="bg-yellow-300 m-1 p-2 rounded-lg">
           <span className="grid grid-cols-2">
-            <div className="col-span-1 mr-1 lg:text-nowrap flex justify-center items-center">
+            <div className="col-span-1 mr-1 lg:text-nowrap flex justify-center items-center bg-yellow-800 rounded">
               De-code Radio
             </div>
             <div className="col-span-1 border-2 bg-black text-white border-green-500 rounded-md">
@@ -45,9 +26,14 @@ export default function musicPlayerController() {
               </div>
             </div>
           </span>
-          <div className="w-full bg-blue-400 rounded mt-1 p-1">
-            <div>Song Name</div>
-            <div>song timer </div>
+          <div className=" bg-blue-400 rounded mt-1 px-1">
+            <div className="overflow-hidden">
+              <div className=" animate-marquee whitespace-nowrap">
+                <span className=" text-white">
+                  Your scrolling radio text goes here. This is a ticker!
+                </span>
+              </div>
+            </div>
           </div>
           <span className="flex flex-row justify-around mt-1">
             <button className="bg-orange-500 hover:bg-blue-500">Rewind</button>
